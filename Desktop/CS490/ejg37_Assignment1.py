2ce83b1d95b950eb0984416336f937adf0524d2b
Eric <ericg8127@gmail.com>
2025-09-07 22:56:33 -0400

    RESTful API Info Submission

---
+++ Desktop/CS490/ejg37_Assignment1.py
@@ -0,0 +1,34 @@
+import json
+import requests
+
+
+def main():
+
+
+    url = "https://student-info-api.netlify.app/.netlify/functions/submit_student_info"
+
+
+
+    data = {
+        "UCID": "ejg37",
+        "first_name": "Eric",
+        "last_name": "Gomez",
+        "github_username": "EJ8127",
+        "discord_username": "oofberic",
+        "favorite_cartoon": "Avatar: The Last Airbender",
+        "favorite_language": "Python",
+        "movie_or_game_or_book": "Interstellar",
+        "section": "101",
+    }
+
+    headers = {"Content-Type": "application/json"}
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        print("Status:", response.status_code)
+        print("Response:", response.text)
+    except requests.exceptions.RequestException as error:
+        print("Error sending request:", error)
+
+if __name__ == "__main__":
+    main()
